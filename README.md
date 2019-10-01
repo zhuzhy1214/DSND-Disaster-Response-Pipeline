d6e32333a5b57e3c3fbf0fabf12d23cda001e00c
zhenyu zhu <zhyzhu@gmail.com>
2019-10-01 12:28:40 -0700

    regulate message one-hot data range to 0-1

--- README.md
+++ README.md
@@ -12,3 +12,44 @@
     `python run.py`
 
 3. Go to http://0.0.0.0:3001/
+
+
+Files:
+- app
+| - template
+| |- master.html  # main page of web app
+| |- go.html  # classification result page of web app
+|- run.py  # Flask file that runs app
+
+- data
+|- disaster_categories.csv  # data to process
+|- disaster_messages.csv  # data to process
+|- process_data.py
+|- DisasterResponse.db   # database to save clean data to
+
+- models
+|- train_classifier.py
+|- disaster_model.pkl  # saved model
+
+- notebooks
+|- ETL Pipeline Preparation.ipynb # etl exploration
+|- ML Pipeline Preparation.ipynb # ML exploration
+
+- README.md
+Required packages:
+flask
+joblib
+jupyter # If you want to view the notebooks
+pandas
+plot.ly
+numpy
+scikit-learn
+sqlalchemy
+
+Data and cleaning details:
+The data in this project comes from a modified version of the figure-eight disaster response data. In general it was pretty clean, the primary transform steps of the ETL are to a remove a few values that don't match up between the categories/messages data and remove a few bad remaining values.
+
+For example the yes/no values are 1/2 in the original data but 0/1 in the provided data. However, a few 2s remained and are replaced with 0s.
+
+Some thoughts on the classifier:
+The distribution of many classes is extremely skewed with very few 1 values. The classifier in this project was not optimized very well to work in this case - in a real world application I would increase the weight of recall in the score significantly in order to catch more of these low representation classes.
--- data/process_data.py
+++ data/process_data.py
@@ -1,17 +1,65 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """loads the message and category data
+    Args:
+        messages_filepath (string): The file path of the messages csv
+        categories_filepath (string): The file path of the categories cv
+    Returns:
+        df (pandas dataframe): The combined messages and categories df
+    """
 
+    messages = pd.read_csv(messages_filepath)
+	categories = pd.read_csv(categories_filepath)
+	df = messages.merge(categories, on = 'id', how='inner')
 
+	return df
+	
+	
 def clean_data(df):
-    pass
+    """Cleans the data:
+        - drops duplicates
+        - removes messages missing classes
+        - cleans up the categories column
+    Args:
+        df (pandas dataframe): combined categories and messages df
+    Returns:
+        df (pandas dataframe): Cleaned dataframe with split categories
+    """
+	
+	categories = df['categories'].str.split(';', expand = True)
+	
+	for column in categories:
+		# set each value to be the last character of the string
+		categories[column] = categories[column].str[-1]
+		
+		# convert column from string to numeric
+		categories[column] = categories[column].astype(int)
+	#regulate some of the '2' value to '1'
+	categories['related']  = categories['related'].apply(lambda x: min(x,1))
+	
+	df.drop(columns= ['categories'], inplace=True)
+	df = pd.concat([df, categories], axis = 1)
+	
+	df.drop_duplicates(inplace=True)
+	
+	return df
 
 
-def save_data(df, database_filename):
-    pass  
+def save_data(df, database_filename, table_name = 'processed_data'):
+    """Saves the resulting data to a sqlite db
+    Args:
+        df (pandas dataframe): The cleaned dataframe
+        database_filename (string): the file path to save the db
+    Returns:
+        None
+    """
 
+	engine = create_engine('sqlite:///'+database_filename)
+	df.to_sql(table_name, engine, index=False)
 
 def main():
     if len(sys.argv) == 4:
@@ -26,9 +74,9 @@ def main():
         df = clean_data(df)
         
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-        save_data(df, database_filepath)
+        save_data(df, database_filepath, 'processed_data')
         
-        print('Cleaned data saved to database!')
+        print('Cleaned data saved to database with table name of {}!'.format('processed_data'))
     
     else:
         print('Please provide the filepaths of the messages and categories '\
